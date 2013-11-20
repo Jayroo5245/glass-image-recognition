cfc9291868b24904af2cb8fdf2b2338456bc5752
sheehanj@lab126.com <sheehanj@lab126.com>
2013-11-19 16:24:28 -0800

    Enhanced VoiceTrigger, now its a two step process

--- src/com/example/glass_image_recognition/MainActivity.java
+++ src/com/example/glass_image_recognition/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.glass_image_recognition;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +15,15 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		TextView textView = (TextView)findViewById(R.id.recognition_text_TextView);
+		Intent intent = getIntent();
+		if(intent != null && intent.getExtras() != null){
+			ArrayList<String> voiceResults = intent.getExtras()
+			        .getStringArrayList(RecognizerIntent.EXTRA_RESULTS);
+			if(textView != null && voiceResults != null && voiceResults.size() > 0){
+				textView.setText(voiceResults.get(0));
+			}
+		}
 	}
 
 	@Override
