1a894f34978f82d853d8431fac1801f45ca2e5fc
sheehanj@lab126.com <sheehanj@lab126.com>
2013-11-19 16:42:32 -0800

    Updated to single step activity launch from VoiceTrigger

--- src/com/example/glass_image_recognition/MainActivity.java
+++ src/com/example/glass_image_recognition/MainActivity.java
@@ -1,16 +1,24 @@
 package com.example.glass_image_recognition;
 
+import java.io.File;
 import java.util.ArrayList;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.speech.RecognizerIntent;
 import android.view.Menu;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	File mImageFile;
+	String mSelectedImagePath;
+	int TAKE_PICTURE = 1001;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -24,6 +32,31 @@ public class MainActivity extends Activity {
 				textView.setText(voiceResults.get(0));
 			}
 		}
+		//imageFromCamera();
+	}
+	
+	public void imageFromCamera() {
+	    Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+	    mImageFile = new File(Environment.getExternalStorageDirectory()+File.separator+"MyApp",  
+	            "PIC"+System.currentTimeMillis()+".jpg");
+	    mSelectedImagePath = mImageFile.getAbsolutePath();
+	    intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(mImageFile));
+	    startActivityForResult(intent, TAKE_PICTURE);
+	}
+	
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    super.onActivityResult(requestCode, resultCode, data);
+	    if (resultCode == RESULT_OK) {
+//	        switch(requestCode) {
+//	        case TAKE_PICTURE:
+//	                    //Launch ImageEdit Activity
+//	            Intent i = new Intent(this, ImageEdit.class);
+//	                    i.putString("imgPath", "mSelectedImagePath");
+//	                    startActivity(i);
+//	            break;
+//	        }
+	    }
 	}
 
 	@Override
